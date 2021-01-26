a9cac055a0af7d6d358ff8225f12dab1da845cf7
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2021-01-26 23:20:24 +0200

    Create nota.cpp

---
+++ cpp/nota.cpp
@@ -0,0 +1,18 @@
+//100 de puncte
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin>>n;
+    
+    if(n<5) {
+        cout<< "corigent";
+    } else {
+        cout << "promovat";
+    }
+    return 0;
+}
