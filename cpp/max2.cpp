8fe77b71f23aaf78010daa90fe9922c3d495936a
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2021-01-26 23:19:51 +0200

    Create max2.cpp

---
+++ cpp/max2.cpp
@@ -0,0 +1,21 @@
+//100 de puncte
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    if(n > m)
+    {
+        cout << n;
+    }
+    else
+    {
+        cout << m;
+    }
+    return 0;
+}
