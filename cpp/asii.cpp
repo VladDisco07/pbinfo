beda984e597fd1f1f3940a7b3b88af233e284055
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2021-01-26 23:04:01 +0200

    Create asii.cpp

---
+++ cpp/asii.cpp
@@ -0,0 +1,13 @@
+//100 de puncte
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << a + b << ' ' << a - b << ' ' << a * b << ' ' << a / b;
+}
