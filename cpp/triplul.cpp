30c9a294408a34804918530c7850a14242a46084
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2021-01-26 23:15:49 +0200

    Create triplul.cpp

---
+++ cpp/triplul.cpp
@@ -0,0 +1,20 @@
+//100 de puncte
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y,necunoscut,rezultat;
+    cin >> x;
+    cin >> y;
+
+    necunoscut =y*3;
+    rezultat=necunoscut-x;
+
+    cout << rezultat;
+
+
+    return 0;
+}
