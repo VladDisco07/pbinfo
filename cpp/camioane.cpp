8e9f1c718da50e47f0b86f4107bfc5f3ca42c6fa
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2021-01-26 23:13:19 +0200

    Create camioane.cpp

---
+++ cpp/camioane.cpp
@@ -0,0 +1,14 @@
+//100 de puncte
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t1,t2,n,m,z;
+    cin>>t1>>t2>>n>>m>>z;
+
+    cout << ((t1*n) + (t2*m))*z;
+    return 0;
+}
