315e0fabc9576615aefb654284f30979072fd29c
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2021-01-26 23:11:43 +0200

    Create parc2.cpp

---
+++ cpp/parc2.cpp
@@ -0,0 +1,16 @@
+//100 de puncte
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,rezultat;
+    cin>>n;
+
+    rezultat = n*n*n*n*n;
+    cout << rezultat;
+
+    return 0;
+}
