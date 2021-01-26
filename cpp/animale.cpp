73b601a9096ce647a443b00fa42a621043637c7c
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2021-01-26 23:12:31 +0200

    Create animale.cpp

---
+++ cpp/animale.cpp
@@ -0,0 +1,18 @@
+//100 de puncte
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int ca, pis, gain, suma;
+    cin >> ca;
+
+    pis = ca*2;
+    gain = pis*2;
+    suma = ca + pis+gain;
+
+    cout << suma << endl;
+    return 0;
+}
