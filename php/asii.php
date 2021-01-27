35f9ef73474b152ef668c1e1ef916bb08278f2d9
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2021-01-27 14:56:44 +0200

    Create asii.php

---
+++ php/asii.php
@@ -0,0 +1,13 @@
+<?php
+    //100 de puncte
+
+    fscanf(STDIN, "%d %d", $a, $b);
+
+    print $a + $b;
+    print " ";
+    print $a - $b;
+    print " ";
+    print $a * $b;
+    print " ";
+    print round($a / $b);
+?>
