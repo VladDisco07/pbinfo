43f48bb5a3d02ef6e3f3a5ef4785095de96adad2
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2021-02-13 12:22:22 +0200

    Create globuri.c

---
+++ c/globuri.c
@@ -0,0 +1,15 @@
+//100 de puncte
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    
+    int globuri_2 = a * 2;
+    int globuri_3 = globuri_2 - 3;
+
+    return 0;
+}
