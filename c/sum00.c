deb25a3d8b694b1387b7284e9b83acc5014293eb
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2021-02-13 12:25:31 +0200

    Create sum00.c

---
+++ c/sum00.c
@@ -0,0 +1,16 @@
+//100 de puncte
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int adunare = a + b;
+
+    printf("%d", adunare);
+
+    return 0;
+}
